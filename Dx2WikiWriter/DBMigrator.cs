2c4f7f003f2587dac51fdf5aefbe8244a4c77811
Justin <witol.justin@hotmail.com>
2019-05-03 17:36:37 -0400

    More MIgration Fixes

--- Dx2WikiWriter/DBMigrator.cs
+++ Dx2WikiWriter/DBMigrator.cs
@@ -27,28 +27,29 @@ namespace Dx2WikiWriter
                     lines += "# " + demon.Name + "\n";
 
                     //Insert Demons
-                    lines += "Insert Into dx2hub.Demons Values (" +
+                    lines += "Insert Into dx2.demons Values (" +
                         "null, " +
                         "\"" + demon.Name + "\"," +
-                        " (Select Race_ID from dx2hub.races where Race_Name = \"" + demon.Race + "\")," +
-                        " (Select AI_ID from dx2hub.races where AI_Name = \"" + demon.Ai + "\")," +
+                        " (Select race_id from dx2.demon_races where race_name = \"" + demon.Race + "\")," +
+                        " (Select ai_id from dx2.demon_ais where ai_name = \"" + demon.Ai + "\")," +
                         " " + demon.Grade + "," +
                         " " + demon.Rarity + "); \n";
 
                     //Insert Resistances
-                    lines += "Insert Into dx2hub.Resistances Values (" +
-                        " (Select Demon_ID from dx2hub.Demons where Demon_Name = \"" + demon.Name + "\")," +
-                        " (Select Resist_ID from dx2hub.Resistance_Type where Resist_Type = \"" + FixResist(demon.Phys) + "\")," +
-                        " (Select Resist_ID from dx2hub.Resistance_Type where Resist_Type = \"" + FixResist(demon.Fire) + "\")," +
-                        " (Select Resist_ID from dx2hub.Resistance_Type where Resist_Type = \"" + FixResist(demon.Ice) + "\")," +
-                        " (Select Resist_ID from dx2hub.Resistance_Type where Resist_Type = \"" + FixResist(demon.Force) + "\")," +
-                        " (Select Resist_ID from dx2hub.Resistance_Type where Resist_Type = \"" + FixResist(demon.Elec) + "\")," +
-                        " (Select Resist_ID from dx2hub.Resistance_Type where Resist_Type = \"" + FixResist(demon.Light) + "\")," +
-                        " (Select Resist_ID from dx2hub.Resistance_Type where Resist_Type = \"" + FixResist(demon.Dark) + "\")); \n";
+                    lines += "Insert Into dx2.demon_resistances Values (" +
+                        "(Select demon_id from dx2.demons where demon_name = \"" + demon.Name + "\"), ";
+                    lines += CreateInsertResist(FixResist(demon.Phys)) + ", ";
+                    lines += CreateInsertResist(FixResist(demon.Fire)) + ", ";
+                    lines += CreateInsertResist(FixResist(demon.Ice)) + ", ";
+                    lines += CreateInsertResist(FixResist(demon.Force)) + ", ";
+                    lines += CreateInsertResist(FixResist(demon.Elec)) + ", ";
+                    lines += CreateInsertResist(FixResist(demon.Light)) + ", ";
+                    lines += CreateInsertResist(FixResist(demon.Dark)) + "); \n";
 
                     //Insert Stats
-                    lines += "Insert Into dx2hub.Stats Values (" +
-                        "50," + demon.Str + "," + demon.Mag + "," + demon.Vit + "," + demon.Agi + "," + demon.Luck + "," + true + "); \n";
+                    lines += "Insert Into dx2.demon_stats Values (" +
+                        "(Select demon_id from dx2.demons where demon_name = \"" + demon.Name + "\"), " +
+                        "50," + demon.HP + "," + demon.Str + "," + demon.Mag + "," + demon.Vit + "," + demon.Agi + "," + demon.Luck + "," + true + "); \n";
 
                     lines += "\n";
                 }
@@ -57,11 +58,19 @@ namespace Dx2WikiWriter
             File.WriteAllText(loadedPath + "/Migration.sql", lines);
         }
 
+        private static string CreateInsertResist(string resist)
+        {
+            if (resist != "")
+                return "(Select resist_id from dx2.demon_resistance_type where resist_type = \"" + resist + "\")";
+
+            return "null";
+        }
+
         private static string FixResist(string resist)
         {
             var fixedResist = resist.Replace("{{ResistColor|1=", "");
             fixedResist = fixedResist.Replace("}}", "");
-            fixedResist = fixedResist.Replace("<nowiki>-</nowiki>", "null");
+            fixedResist = fixedResist.Replace("<nowiki>-</nowiki>", "");
 
             return fixedResist;
         }
